a48ee3cb70bf853c722716b000e0635b9dc78fd6
taku3des <148851159+taku3des@users.noreply.github.com>
2024-04-23 17:32:40 +0900

    サーボ

---
+++ program/sarvo/sarvo.ino
@@ -0,0 +1,24 @@
+#include<Servo.h>
+Servo makitori;
+Servo agesage;
+int Left_1, Left_2,Right_1, Right_2;
+float kakudo = 0;
+float kakudo2 = 0;
+void setup() {
+  makitori.attach(1);
+  agesage.attach(1);
+}
+void loop() {
+  if(Left_1 = 1){
+    kakudo + 0.01f;
+    makitori.write(kakudo);}
+  if(Right_1 = 1){
+    kakudo - 0.01f;
+    makitori.write(kakudo);}
+  if(Left_2 = 1){
+    kakudo2 + 0.01f;
+    agesage.write(kakudo2);}
+  if(Right_2 = 1){
+    kakudo2 - 0.01f;
+    agesage.write(kakudo2);}
+}
