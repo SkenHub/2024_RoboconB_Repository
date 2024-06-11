3691555d154a3fefb24ef4addae58d702b4ee5b5
hotaka-mt <hhhotaka2004@gmail.com>
2024-06-12 01:01:46 +0900

    ロボット1のROSワークスペース追加

---
+++ program/R1_ros_ws/build/CalcuPkg/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/hotaka/2024_RoboconB_Repository/program/R1_ros_ws/install/CalcuPkg'
---
+++ program/R1_ros_ws/build/R1_pkg/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/hotaka/robocon2024/install/R1_pkg'
