a18acc585f0876dfd9f1428a120ca4c5770f7c3f
おやすみ世界 <137624271+OyasumiSekai@users.noreply.github.com>
2024-06-22 17:56:51 +0900

    送信側プログラム

--- program/KarekoR2Controller/KarekoR2Controller.ino
+++ program/KarekoR2Controller/KarekoR2Controller.ino
@@ -3,18 +3,17 @@
 
 SoftwareSerial IM920Serial(8, 9);  //受信 RX をピン 8、送信 TX をピン 9 に割り当て
 IM920 im920(IM920Serial);
-uint8_t tx_data[8] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+//uint8_t tx_data[8] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
 
-float Left_X,Left_Y,Right_X,Right_Y,Left_1,Left_2,Right_1,Right_2;
 
-const int switch1 = 2;
+const int switch1 = 2;//PushなSWのピン番号
 const int switch2 = 3;
 const int switch3 = 4;
 const int switch4 = 5;
-const int switch5 = 6;
+const int switch5 = 6;4
 const int switch6 = 7;
 
-byte switchState1 = 0;
+byte switchState1 = 0;//押されているか否かを定義
 byte switchState2 = 0;
 byte switchState3 = 0;
 byte switchState4 = 0;
@@ -23,7 +22,7 @@ byte switchState6 = 0;
 
 void setup() {
 
-  pinMode(switch1,INPUT_PULLUP);
+  pinMode(switch1,INPUT_PULLUP);//ピン定義
   pinMode(switch2,INPUT_PULLUP);
   pinMode(switch3,INPUT_PULLUP);
   pinMode(switch4,INPUT_PULLUP);
@@ -38,7 +37,7 @@ void setup() {
 void loop() {
 byte send_data = 0b00000000;
 
-switchState1 = (digitalRead(switch1) == 1 ) ? 0 : 1;
+switchState1 = (digitalRead(switch1) == 1 ) ? 0 : 1;//INPUT_PULLUPだと押されている時に0になるのがキモいからそれを反転させてる
 switchState2 = (digitalRead(switch2) == 1 ) ? 0 : 1;
 switchState3 = (digitalRead(switch3) == 1 ) ? 0 : 1;
 switchState4 = (digitalRead(switch4) == 1 ) ? 0 : 1;
@@ -81,18 +80,23 @@ switchState6 = (digitalRead(switch6) == 1 ) ? 0 : 1;
   }
   if(switchState5 == 1){
     send_data = send_data | 0b00100000;
-    Serial.print("L2");
+    Serial.print("L");
   }else{
     if(switchState6 == 1){
       send_data = send_data | 0b00010000;
-      Serial.print("R2");
+      Serial.print("R");
     }
   }
 
 
-Serial.println(send_data);
+Serial.println(send_data,HEX);
 
 //この辺は要調整
-uint8_t tx_data[8] = {send_data,send_data,send_data,send_data,send_data,send_data,send_data,send_data};
+uint8_t tx_data[8] = {0,0,0,0,0,0,0,send_data};
+//uint8_t tx_data[8] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+
 im920.write(tx_data,Bytes8);
+//im920.write(tx_data,send_data);
+
+//delay(92);
 }
