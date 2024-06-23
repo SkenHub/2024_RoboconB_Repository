c9699f7a68eef08e87fa8d85b3dfbc046b0e4812
おやすみ世界 <137624271+OyasumiSekai@users.noreply.github.com>
2024-06-23 17:44:30 +0900

    大量にあった謎のコメントアウトを除去
    なぜ絶対使わないであろうコードがコメントアウトされた状態で残ってたんだ?

--- program/kanekosan_arduinoNano/kanekosan_arduinoNano.ino
+++ program/kanekosan_arduinoNano/kanekosan_arduinoNano.ino
@@ -1,12 +1,3 @@
-
-/*
-R6/06/23 16:30時点での最新版
-*/
-
-
-
-
-//#include <Ps3Controller.h>
 #include <SoftwareSerial.h>
 #include <Servo.h>
 #include "IM920.h"
@@ -14,40 +5,31 @@ R6/06/23 16:30時点での最新版
 SoftwareSerial IM920Serial(8,9);
 ConvertIntFloat convert;
 IM920 im920(IM920Serial);
+
 uint8_t tx_data[8] = {0xFF,0,0,0,0,0xA0,0,0};
 uint8_t rx_data[8] = {0,0,0,0,0,0,0,0};
-//int date1,date2,date3,date4,date5,date6,date7,date8;
-//ピン番号にint型は贅沢かも?
+
 constexpr byte S1 = 9;//Servo1のピン番号を記入
 float ServoDeg = 0.00;//Servo角度制御用変数の初期化
 float ServoLimitDeg = 130.00;//Servo最大角
 Servo servo;
 uint8_t data[6];
-//float Left_X,Left_Y,Right_X,Right_Y,Left_1,Left_2,Right_1,Right_2;
-//int Left_kaiten, Right_kaiten, Zensin, Kousin;
-////////////////////////////////////////////////////////////////////////////////////////
-//doubleから変えてみる(もしかしたら治るかも?)
+
 float Right_Motor_Output = 0;//左モーター
 float Left_Motor_Output = 0;//右モーター
-////////////////////////////////////////////////////////////////////////////////////////
+
 constexpr byte Right_Motor_P1 = 12;
 constexpr byte Right_Motor_P2 = 13;
-constexpr int Right_Motor_PWM =6;//4
+constexpr int Right_Motor_PWM =6;
 constexpr byte Left_Motor_P1 = 14;
-constexpr byte Left_Motor_P2 = 2;//23から変更してみる
-constexpr int Left_Motor_PWM =12;//2
-//////////////////////////////////////////////////////////////////////////////////////////
+constexpr byte Left_Motor_P2 = 2;
+constexpr int Left_Motor_PWM =12;
 
 
-//////////////////////////////////////////////////////////////////////////////////////////
 void setup() {
   Serial.begin(115200);
 
   Serial.println("Hello");
-//  Serial.println("go");
-/////////////////////////////////////////////////////////////////////////////////////////
-
-/////////////////////////////////////////////////////////////////////////////////////////
 
   servo.attach(S1,510,2400);
   im920.init();
@@ -58,30 +40,13 @@ void setup() {
 }
 
 
-
-
-
 void loop() {
   
-/////////////////IM920との通信
 
+//IM920との通信
   long int rx[2];
   uint16_t NN;
   im920.read(&NN,rx_data,Bytes8);
-
-/*  for(int i=0; i<2; i++){
-    for(int j=0; j<4; j++){
-      convert.uint8_val[j] = rx_data[i*4+j];
-    }
-    rx[i] = convert.float_val;
-  }
-  for(int i=0; i<2; i++){
-    Serial.print(rx[i]);
-    Serial.print("  ");
-  }
-  Serial.println();*/
-
-///////////////////////////////////////////////////////////////////////////
 
   byte firstByte = rx_data[0];
   int bitdata[8];
@@ -91,38 +56,9 @@ void loop() {
 
   Serial.println(rx_data[0],BIN); 
 
-///////////////////////////////////////////////////////////////////////////
-
-/*  if (Ps3.isConnected()){
-    Left_X = Ps3.data.analog.stick.lx;
-    Left_Y = -Ps3.data.analog.stick.ly;
-    Right_X = Ps3.data.analog.stick.rx;
-    Right_Y = -Ps3.data.analog.stick.ry;
-    Left_1 = Ps3.data.button.l1;
-    Left_2 = Ps3.data.button.l2;
-    Right_1 = Ps3.data.button.r1;
-    Right_2 = Ps3.data.button.r2;
-    Serial.print(Left_X);
-    Serial.print(" ");
-    Serial.print(Left_Y);
-    Serial.print(" ");
-    Serial.print(Right_X);
-    Serial.print(" ");
-    Serial.print(Right_Y);
-    Serial.print(Right_Motor_Output);
-    Serial.print(Left_Motor_Output);
-  }
 
 
-///////////////////////////////////////////////////////////////////////////////////////////
-*/
-
-
-
-
-
-
-  if(bitdata[0] = 1){  //いじりました
+  if(bitdata[0] = 1){
     Left_Motor_Output = 200.0;
     if(bitdata[2] = 1){
       Left_Motor_Output = -200.0;
@@ -154,21 +90,15 @@ void loop() {
     Right_Motor_Output = Right_Motor_Output * 0.7;
   }  
 
-//////////////////////////////////////////////////////////////////////////////////////////
-
 
   if(Right_Motor_Output > 0){
     analogWrite(Right_Motor_P1,0);
     analogWrite(Right_Motor_P2,Right_Motor_Output);
-    //Serial.print("Right_M_P");
-    //Serial.println(Right_Motor_Output);
   }
 
   else if(Right_Motor_Output < 0){
     analogWrite(Right_Motor_P1,Right_Motor_Output);
     analogWrite(Right_Motor_P2,0);
-    //Serial.print("Right_M_M");
-    //Serial.println(Right_Motor_Output);
   }
 
   else if(Right_Motor_Output == 0) {
@@ -179,20 +109,16 @@ void loop() {
   if(Left_Motor_Output > 0){
     analogWrite(Left_Motor_P1,Left_Motor_Output);
     analogWrite(Left_Motor_P2,0);
-    //Serial.print("Left_M_P");
-    //Serial.println(Left_Motor_Output);
   }
 
   else if(Left_Motor_Output < 0){
     analogWrite(Left_Motor_P1,0);
     analogWrite(Left_Motor_P2,Left_Motor_Output);
-    //Serial.print("Left_M_M");
   }
 
   else if(Left_Motor_Output == 0){
     analogWrite(Left_Motor_P1,0);
     analogWrite(Left_Motor_P2,0);
-    //Serial.println(Left_Motor_Output);
   }
 
 
@@ -205,8 +131,6 @@ void loop() {
       ServoDeg = ServoDeg + 0.5;}
 
     servo.write(ServoDeg); //ServoDeg度に回す
-    //Serial.println(ServoDeg);
-    //Serial.println(Ps3.data.button.l2);
   }
 
   if(bitdata[7] == 1){//Rトリガー検知
@@ -214,16 +138,14 @@ void loop() {
       ServoDeg = ServoDeg - 0.5;}
 
   servo.write(ServoDeg);
-  //Serial.println(ServoDeg);
-  //Serial.println(Ps3.data.button.r2);
   }
 
 
 
-  //Serial.print("Left_motor");
-  //Serial.println(Left_Motor_Output);
-  //Serial.print("Right_motor");
-  //Serial.println(Right_Motor_Output);
+  Serial.print("Left_motor");
+  Serial.println(Left_Motor_Output);
+  Serial.print("Right_motor");
+  Serial.println(Right_Motor_Output);
 
  
 
