7c02c2d0353d686fd4546873ebcf75347b31912e
taku3des <148851159+taku3des@users.noreply.github.com>
2024-09-24 16:16:47 +0900

    コメントアウトの削除など

--- program/R2_Controler/R2_Controler.ino
+++ program/R2_Controler/R2_Controler.ino
@@ -4,27 +4,9 @@
 SoftwareSerial IM920Serial(22,23);  //受信 RX をピン 8、送信 TX をピン 9 に割り当て
 IM920 im920(IM920Serial);
 uint8_t tx_data[8] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
-/*
-const int switch1 = 2;//PushなSWのピン番号
-const int switch2 = 3;
-const int switch3 = 4;
-const int switch4 = 5;
-const int switch5 = 6;
-const int switch6 = 7;
-const int switch7 = 6;
-const int switch8 = 7;
-*/
-//const int sw[8] = {2,3,4,5,6,7,0,0};
-const int sw[11] = {35,26,33,25,17,18,5,19,34,32,16}; 
+const int sw[11] = {35,26,33,25,17,18,5,19,34,32,16};
+constexpr byte LED = 12;
 
-/*
-byte switchState1 = 0;//押されているか否かを定義
-byte switchState2 = 0;
-byte switchState3 = 0;
-byte switchState4 = 0;
-byte switchState5 = 0;
-byte switchState6 = 0;
-*/
 uint8_t switchState = 0;
 uint8_t switchState2 = 0;
 
@@ -32,6 +14,7 @@ void setup() {
   for(int i=0; i<11; i++){
     pinMode(sw[i],INPUT_PULLUP);//ピン定義
   }
+  pinMode(LED,OUTPUT);
   Serial.begin(19200);
   im920.init();
 }
@@ -43,6 +26,10 @@ void loop() {
   for(int i=0; i<3; i++) switchState2 |= ((digitalRead(sw[i+8]))? 0 : 1) << i;
   int kae = 0;
   kae = digitalRead(sw[10]);
+  if(switchState&0x40){
+    digitalWrite(LED,HIGH);}
+  else{
+    digitalWrite(LED,LOW);}
   tx_data[0] = switchState;
   tx_data[1] = switchState2;
   //Serial.println(sw[5]);
@@ -58,70 +45,4 @@ void loop() {
   }
   //Serial.println(kae);
   Serial.println();
-/*
-byte send_data = 0b00000000;
-
-switchState1 = (digitalRead(switch1) == 1 ) ? 0 : 1;//INPUT_PULLUPだと押されている時に0になるのがキモいからそれを反転させてる
-switchState2 = (digitalRead(switch2) == 1 ) ? 0 : 1;
-switchState3 = (digitalRead(switch3) == 1 ) ? 0 : 1;
-switchState4 = (digitalRead(switch4) == 1 ) ? 0 : 1;
-switchState5 = (digitalRead(switch5) == 1 ) ? 0 : 1;
-switchState6 = (digitalRead(switch6) == 1 ) ? 0 : 1;
-
-
-  if(switchState1 == 1){//全進B
-    send_data = send_data | 0b00000011;
-    Serial.print("up");
-  }
-  if(switchState2 == 1){//右B
-    send_data = send_data | 0b00000001;
-  }
-  if(switchState3 == 1){//左B
-    send_data = send_data | 0b00000010;
-  }
-
-
- 
-
-  if(switchState4 == 1){
-
-    if( switchState3 == 1){//後退+左B
-      send_data = send_data | 0b00001010;
-      Serial.print("Left");
-
-    }else{
-
-      if(switchState2 == 1){//後退+右B
-        send_data = send_data | 0b00000101;
-        Serial.print("Right");
-
-      }else{//後退B
-        send_data = send_data | 0b00001111;
-        Serial.print("down");
-
-      }
-    }    
-  }
-  if(switchState5 == 1){//Servo+
-    send_data = send_data | 0b00100000;
-    Serial.print("L");
-  }else{
-    if(switchState6 == 1){//Servo-
-      send_data = send_data | 0b00010000;
-      Serial.print("R");
-    }
-  }
-
-
-Serial.println(send_data,HEX);
-
-//この辺は要調整
-uint8_t tx_data[8] = {send_data,send_data,send_data,send_data,send_data,send_data,send_data,send_data};
-//uint8_t tx_data[8] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
-
-im920.write(tx_data,Bytes8);
-//im920.write(tx_data,send_data);
-
-//delay(92);
-*/
 }
--- program/R2_Enkaku/R2_Enkaku.ino
+++ program/R2_Enkaku/R2_Enkaku.ino
@@ -6,19 +6,19 @@ SoftwareSerial IM920Serial(9,10);
 ConvertIntFloat convert;
 IM920 im920(IM920Serial);
 
-uint8_t tx_data[8] = {0xFF,0,0,0,0,0xA0,0,0};
+//uint8_t tx_data[8] = {0xFF,0,0,0,0,0xA0,0,0};
 uint8_t rx_data[8] = {0,0,0,0,0,0,0,0};
 
 constexpr byte S1 = 6;//Servo1のピン番号を記入
 constexpr byte S2 = 5;
-float ServoDeg = 180.00;//Servo角度制御用変数の初期化
-float servo_ht = 0.00;
+float ServoDeg;// = 180.00;//Servo角度制御用変数の初期化
+float servo_ht = 60.00;
 float ServoLimitDeg = 180.00;//Servo最大角
 Servo servo;
 Servo servo2;
 float R_Mtr = 0;//左モーター
 float L_Mtr = 0;//右モーター
-
+int sarvo_kiri = 0;
 constexpr byte Right_Motor_P1 = A0;
 constexpr byte Right_Motor_P2 = A1;
 constexpr byte Left_Motor_P1 = A4;
@@ -44,36 +44,34 @@ void loop() {
   byte firstByte = rx_data[0];
   Serial.println(firstByte,HEX);
   
-   R_Mtr=0;
+  R_Mtr=0;
   L_Mtr=0;
   if(firstByte&0x01){
-    R_Mtr -= 150;
-    L_Mtr -= 150;
+    R_Mtr -= 200;
+    L_Mtr -= 200;
   }
   if(firstByte&0x02){
-    R_Mtr += 150;
-    L_Mtr += 150;
+    R_Mtr += 200;
+    L_Mtr += 200;
   }
   if(firstByte&0x04){
-    R_Mtr += 150;
-    L_Mtr -= 150;
+    R_Mtr += 200;
+    L_Mtr -= 200;
   }
   if(firstByte&0x08){
-    R_Mtr -= 150;
-    L_Mtr += 150;
+    R_Mtr -= 200;
+    L_Mtr += 200;
   }
   if(firstByte&0x40){
-    R_Mtr *= 1.25;
-    L_Mtr *= 1.25;  
-  }
-  if(firstByte&0x80){
     R_Mtr *= -1;
-    L_Mtr *= -1;
-    servo_ht = 150.0;
+   L_Mtr *= -1; 
+  } 
+  if(firstByte&0x80){
+    servo_ht = 0.00;
     servo2.write(servo_ht);
   }
   else{
-    servo_ht = 90.0;
+    servo_ht = 180.00;
     servo2.write(servo_ht);
   }
   //Serial.println(servo_ht);
@@ -93,13 +91,17 @@ void loop() {
   }
   
   if(firstByte&0x10){//Lトリガー検知
-    if(ServoDeg < ServoLimitDeg){
-      ServoDeg += 0.1;}
+    /*if(ServoDeg < ServoLimitDeg){
+      ServoDeg += 0.1;}*/
+    ServoDeg = 180.00;
   }
-  if(firstByte&0x20){//Rトリガー検知
+  else{
+    ServoDeg = 0.00;
+  }
+  /*if(firstByte&0x20){//Rトリガー検知
     if(ServoDeg > 0.00){
       ServoDeg -= 0.1;}
-  } 
+  } */
   //Serial.println(ServoDeg);
   //Serial.println(servo_ht);
   servo.write(ServoDeg);
